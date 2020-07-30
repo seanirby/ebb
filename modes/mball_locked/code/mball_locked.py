07c3d063e38ea55eb48ef646159b38d1fee73aa7
Sean Irby <sean.t.irby@gmail.com>
2020-07-30 05:37:26 -0700

    update drops, lanes, right targets

--- modes/mball_locked/code/mball_locked.py
+++ modes/mball_locked/code/mball_locked.py
@@ -1,17 +1,19 @@
 from mpf.core.mode import Mode
 
+NUM_DROPS = 3
+
 class MballLocked(Mode):
     def mode_start(self, **kwargs):
         self.init_drops()
 
     def init_drops(self):
-        for i in range(1, 3):
+        for i in range(0, NUM_DROPS - 1):
             event = "deactivate_upper_drop_{}".format(i)
             # self.machine.events.post(event)
             # adding a delay because it was necessary for vpx
             self.delay.add(500, self.machine.events.post, None, event=event)
 
-        event = "activate_upper_drop_3"
+        event = "activate_upper_drop_2"
         self.delay.add(500, self.machine.events.post, None, event=event)
 
         
--- modes/mball_qualifying/code/mball_qualifying.py
+++ modes/mball_qualifying/code/mball_qualifying.py
@@ -1,5 +1,7 @@
 from mpf.core.mode import Mode
 
+NUM_DROPS = 3
+
 class MballQualifying(Mode):
     def mode_start(self, **kwargs):
         print("IN MODE START WHOOPIEE")
@@ -9,12 +11,12 @@ class MballQualifying(Mode):
         drops_to_knock_down = self.player.mball_progress
 
         if drops_to_knock_down == 0:
-            for i in range(1, 4):
+            for i in range(0, NUM_DROPS):
                 if not(self.machine.diverters["div_drops_upper_{}".format(i)].active):
                     event = "activate_upper_drop_{}".format(i)
                     self.delay.add(500, self.machine.events.post, None, event=event)
         else:
             # need to set UP drops too
-            for i in range(1, drops_to_knock_down + 1):
+            for i in range(0, drops_to_knock_down):
                 event = "deactivate_upper_drop_{}".format(i)
                 self.delay.add(500, self.machine.events.post, None, event=event)
