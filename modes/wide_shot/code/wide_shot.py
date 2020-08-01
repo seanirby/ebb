5a7e7ebea91ce8fa09904dd3088321d3595dbb79
Sean Irby <sean.t.irby@gmail.com>
2020-08-01 04:21:23 -0700

    wide shot

--- modes/wide_shot/code/wide_shot.py
+++ modes/wide_shot/code/wide_shot.py
@@ -7,33 +7,45 @@ NUM_TARGETS = 7
 
 class WideShot(Mode):
     def mode_start(self, **kwargs):
-        self.add_mode_event_handler("s_left_flipper_active", self.handle_move, direction=-1)
-        self.add_mode_event_handler("s_right_flipper_active", self.handle_move, direction=1)
+        self.add_mode_event_handler("s_left_flipper_active", self.handle_move, direction=1)
+        self.add_mode_event_handler("s_right_flipper_active", self.handle_move, direction=-1)
         self.add_mode_event_handler("wide_shot_advance_progress", self.handle_progress_change)
         self.add_mode_event_handler("wide_shot_collected", self.handle_collected)
 
     def handle_move(self, **kwargs):
         direction = kwargs.get("direction")
+        progress = self.player["wide_shot_progress"]
+        position = self.player["wide_shot_position"]
+
+        if progress > 0:
+            self.player["wide_shot_position"] = (position + direction) % NUM_TARGETS
+            self.update_lights()
 
     def handle_progress_change(self, **kwargs):
-        self.player["wide_shot_advance_progress"] += 1
+        self.player["wide_shot_progress"] += 1
         self.update_lights()
 
     def update_lights(self):
         pos = self.player["wide_shot_position"]
         progress = self.player["wide_shot_progress"]
-        for i in range(pos - progress, pos + progress + 1):
-            shot = None
-            if i == pos:
-                shot = self.machine.shots["sh_wsl_{}".format(i)]
-                shot.jump(ON)
-            else: 
-                adj_i = (i % NUM_TARGETS) + 1
-                shot = self.machine.shots["sh_wsl_{}".format(adj_i)]
-                shot.jump(FLASH)
-            
-            
+        active = list(map(lambda x: x % NUM_TARGETS, list(range(pos-progress, pos+progress+1))))
 
+        for i in range(0, NUM_TARGETS):
+            shot = self.machine.shots["sh_wsl_{}".format(i)]
+            if i == pos:
+                shot.jump(WSL_ON)
+            elif i in active: 
+                shot.jump(WSL_FLASH)
+            else:
+                shot.jump(WSL_OFF)
+            
     def handle_collected(self, **kwargs):
-        
-    
+        # reset progress
+        self.player["wide_shot_progress"] = 0
+
+        # reset wide shot target
+        shot = self.machine.shots["sh_wide_shot"].jump(0)
+
+        # reset wsl indicators
+        for i in range(0, NUM_TARGETS):
+            self.machine.shots["sh_wsl_{}".format(i)].jump(WSL_OFF)
