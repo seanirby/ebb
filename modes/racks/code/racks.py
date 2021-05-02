5e6e71830e949eef78dcbacebc0f5edc25040241
Sean Irby <sean.t.irby@gmail.com>
2021-05-02 16:18:33 -0700

    basic multiball implementation

--- modes/racks/code/racks.py
+++ modes/racks/code/racks.py
@@ -13,7 +13,8 @@ class Racks(Mode):
         for i in range(0, NUM_TARGETS):
             self.add_mode_event_handler("sh_tl_{}_hit".format(i), self.handle_target_hit, target_number=i)
 
-        self.add_mode_event_handler("sh_rack_collect_hit", self.handle_rack_collected)
+        self.add_mode_event_handler("timer_smart_drop_left_complete", self.update_if_rack_can_be_collected)
+        self.add_mode_event_handler("sh_racks_collect_hit", self.handle_rack_collected)
 
     def init_lights(self):
         for i in range(0, NUM_TARGETS):
@@ -30,21 +31,13 @@ class Racks(Mode):
     
     def handle_target_hit(self, **kwargs):
         target_number = kwargs.get("target_number")
-        # wide_shot = self.machine.shots["sh_wsl_{}".format(target_number)]
-
         targets=[target_number]
-
-        # if wide_shot.state_name == "on":
-        #     progress = self.player["wide_shot_progress"]
-        #     self.machine.events.post("wide_shot_collected")
-        #     # TODO: dedupe the formula below
-        #     targets = list(map(lambda x: x % NUM_TARGETS, list(range(target_number-progress, target_number+progress+1))))
 
         for target in targets:
             self.update_target_progress(target)
             self.update_show(target)
 
-        self.update_if_rack_collected()
+        self.update_if_rack_can_be_collected()
 
     def handle_rack_collected(self, **kwargs):
         # update score
@@ -55,9 +48,9 @@ class Racks(Mode):
         # update target lights
         self.init_lights()
         # disable rack collect shot
-        self.machine.events.post("disable_rack_collect")
+        self.machine.events.post("racks_rack_collected")
 
-        # potentially update difficulty
+        # TODO: potentially update difficulty
         # play a show
 
     def update_target_progress(self, target_number):
@@ -71,7 +64,7 @@ class Racks(Mode):
             shot_name = "l_{}_{}".format(target_number, i)
             self.machine.shots[shot_name].jump(ON)
 
-    def update_if_rack_collected(self):
+    def update_if_rack_can_be_collected(self, **kwargs):
         update = True
         for i in range(0, NUM_TARGETS):
             progress = self.player["tl_{}_progress".format(i)]
@@ -80,7 +73,7 @@ class Racks(Mode):
                 break
 
         if update:
-            self.machine.events.post("enable_rack_collect")
+            self.machine.events.post("racks_enable_qualify_collect")
         
         
 
